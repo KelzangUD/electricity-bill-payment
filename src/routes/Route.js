c79afd4d11bfd3710ebc64aed4be641037bea56b
KelzangUD <kuding755@gmail.com>
2025-08-28 11:30:54 +0600

    docker set up

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -61,8 +61,7 @@ const Route = async (
         return response;
       }
     } catch (error) {
-      console.log(error)
-      if (error?.response?.status === 403) {
+      if (error?.status === 403) {
         try {
           const refresh_token = localStorage.getItem("refresh_token");
           if (!refresh_token) {
@@ -76,11 +75,14 @@ const Route = async (
               Authorization: `Bearer ${refresh_token}`,
             },
           };
+          console.log(refreshOptions)
           const refreshResponse = await axios(
             `${apiUrl}/api/v1/auth/refresh-token`,
             refreshOptions
           );
+          console.log("refresh Response",  refreshResponse)
           if (refreshResponse?.status === 200) {
+            console.log("Token refreshed:", refreshResponse?.data);
             localStorage.setItem(
               "access_token",
               refreshResponse?.data?.access_token
